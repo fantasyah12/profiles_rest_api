b0bad7719c6f7816cde698063be978868c6aba3b
fantasyah12 <fantasyah12@gmail.com>
2021-05-20 08:45:05 +0700

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,14 +1,20 @@
-#from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
+
 # Create your views here.
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -30,9 +36,10 @@ class HelloApiView(APIView):
             return Response({'message': message})
         else:
             return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
             )
+
     def put(self, request, pk=None):
         """Hanlde updating an project"""
         return Response({'method': 'PUT'})
@@ -44,6 +51,8 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
     serializer_class = serializers.HelloSerializer
@@ -56,6 +65,7 @@ class HelloViewSet(viewsets.ViewSet):
             'Provides more functionality with less code',
         ]
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
     def create(self, request):
         """Create a new hello message"""
         serializer = self.serializer_class(data=request.data)
@@ -69,6 +79,7 @@ class HelloViewSet(viewsets.ViewSet):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
+
     def retrieve(self, request, pk=None):
         """ Handle getting an object by its ID"""
         return Response({'http_method': 'GET'})
@@ -84,3 +95,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """ Handle getting an object by its ID"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
